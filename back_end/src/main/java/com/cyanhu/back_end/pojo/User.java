5b63cea4f81b1c4ae2c9cd7459fa80b63c0634dc
CyanHu <1205430144@qq.com>
2023-04-15 00:28:55 +0800

    连接数据库

---
+++ back_end/src/main/java/com/cyanhu/back_end/pojo/User.java
@@ -0,0 +1,17 @@
+package com.cyanhu.back_end.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @TableId(value = "id", type = IdType.AUTO)
+    Integer id;
+    String username;
+    String password;
+}
